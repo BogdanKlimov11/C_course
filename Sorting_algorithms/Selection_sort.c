1c03db4b17a8d5ffa2122e6649e6dbb869ee85ae
Bogdan Klimov <136115919+BogdanKlimov11@users.noreply.github.com>
2024-03-14 08:31:14 +0300

    Create Selection_sort.c

---
+++ Sorting_algorithms/Selection_sort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void selection_sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
