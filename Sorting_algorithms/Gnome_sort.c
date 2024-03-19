038fd173402a50498cb17e7ab1ced293f2862a5c
Bogdan Klimov <136115919+BogdanKlimov11@users.noreply.github.com>
2024-03-19 17:06:03 +0300

    Create Gnome_sort.c

---
+++ Sorting_algorithms/Gnome_sort.c
@@ -0,0 +1,14 @@
+void gnome_sort(int arr[], int n) {
+    int index = 0;
+
+    while (index < n) {
+        if (index == 0 || arr[index] >= arr[index - 1]) {
+            index++;
+        } else {
+            int temp = arr[index];
+            arr[index] = arr[index - 1];
+            arr[index - 1] = temp;
+            index--;
+        }
+    }
+}
